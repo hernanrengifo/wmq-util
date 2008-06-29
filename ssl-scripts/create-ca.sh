4b0a0f31cccdb622064e537e5b279a3b646feba1
protocol7 <protocol7@8f34fe57-393b-0410-9e6a-a161080426ea>
2008-06-29 20:37:50 +0000

    Escaping correctly

--- ssl-scripts/create-ca.sh
+++ ssl-scripts/create-ca.sh
@@ -9,7 +9,7 @@
 # Below, you should change some general configuration
 #
 # The DN to use
-dn=CN=Example CA cert,O=ACME,C=SE
+dn="CN=Example CA cert,O=ACME,C=SE"
 
 # Expiration time in days (e.g. 5*365 => 1825)
 expiration=3650
@@ -17,6 +17,6 @@ expiration=3650
 pw=$1
 
 gsk7cmd -keydb -create -db ca_key.kdb -pw $pw -type cms -expire $expiration
-gsk7cmd -cert -create -db ca_key.kdb -pw $pw -label "CA_Cert" -dn "$dn" -expire $expiration
+gsk7cmd -cert -create -db ca_key.kdb -pw $pw -label "CA_Cert" -dn $dn -expire $expiration
 gsk7cmd -cert -extract -db ca_key.kdb -pw $pw -label "CA_Cert" -target ca_cert.crt
 
